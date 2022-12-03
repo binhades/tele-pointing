11a0a5abcf41d4f7a34f0867af08c82f242f4e7a
Bin Liu <binghades@gmail.com>
2022-12-03 08:50:38 +0800

    small update, catalog_sky_dist looks better

--- plot_catalog_sky_dist.py
+++ plot_catalog_sky_dist.py
@@ -27,43 +27,6 @@ def load_catalog(file_cata):
     sour_list = np.genfromtxt(file_cata,delimiter=',',skip_header=1,dtype=None,names=hdr,encoding=None)
     return sour_list
 
-def plot_hist(flux,n_bins=200, file_out=None,fext='png',isshow=True,plottype='line',xmin=0,xmax=5,fontsize='xx-large',xlabel='Flux [Jy]',ylabel='Counts'):
-
-    fig, axs = plt.subplots(1, 2, tight_layout=True, figsize=(12,5))
-    
-    # N is the count in each bin, bins is the lower-limit of the bin
-    N, bins, patches = axs[0].hist(flux, bins=n_bins)
-    
-    # We'll color code by height, but you could use any scalar
-    fracs = N / N.max()
-    
-    # we need to normalize the data to 0..1 for the full range of the colormap
-    norm = colors.Normalize(fracs.min(), fracs.max())
-    
-    # Now, we'll loop through our objects and set the color of each accordingly
-    for thisfrac, thispatch in zip(fracs, patches):
-        color = plt.cm.viridis(norm(thisfrac))
-        thispatch.set_facecolor(color)
-    
-    # We can also normalize our inputs by the total number of counts
-    axs[1].hist(flux, bins=n_bins, density=True)
-    
-    # Now we format the y-axis to display percentage
-    axs[1].yaxis.set_major_formatter(PercentFormatter(xmax=5))
-    axs[0].set_xlim([xmin,xmax])
-    axs[1].set_xlim([xmin,xmax])
-    axs[0].set_xlabel(xlabel,fontsize=fontsize)
-    axs[1].set_xlabel(xlabel,fontsize=fontsize)
-    axs[0].set_ylabel(ylabel,fontsize=fontsize)
-    axs[0].tick_params(axis='both', labelsize=fontsize) #which='minor'
-    axs[1].tick_params(axis='both', labelsize=fontsize) #which='minor'
-    fig.suptitle('VLA Ku-band (2cm) Calibrator Flux Distribution',fontsize=fontsize)
-    if file_out is not None:
-        plt.savefig(file_out,format=fext,dpi=300)
-    if isshow:
-        plt.show()
-    plt.close()
-    return 0
 #===================
 def plot_equ_sky_scatter(sources,size=None,colorInd=None,file_out=None,isshow=True,fontsize='x-large',plotGP=True,ax_factor=1):
 
@@ -85,12 +48,10 @@ def plot_equ_sky_scatter(sources,size=None,colorInd=None,file_out=None,isshow=Tr
         equp = galp.transform_to('icrs')
         equm = galm.transform_to('icrs')
         ax.scatter(ax_factor*gc_equ.ra.wrap_at(180*u.deg).radian, gc_equ.dec.wrap_at(180*u.deg).radian,marker='*',s=100,color='black')
-        ax.plot(ax_factor*equ0.ra.wrap_at(180*u.deg).radian, equ0.dec.wrap_at(180*u.deg).radian, alpha=0.5,color='green',linestyle='-')
-        ax.plot(ax_factor*equp.ra.wrap_at(180*u.deg).radian, equp.dec.wrap_at(180*u.deg).radian, alpha=0.5,color='tab:green',linestyle='--')
-        ax.plot(ax_factor*equm.ra.wrap_at(180*u.deg).radian, equm.dec.wrap_at(180*u.deg).radian, alpha=0.5,color='tab:green',linestyle='--')
+        ax.plot(ax_factor*equ0.ra.wrap_at(180*u.deg).radian, equ0.dec.wrap_at(180*u.deg).radian, alpha=0.5, zorder=1, color='green',     linestyle='-' )
+        ax.plot(ax_factor*equp.ra.wrap_at(180*u.deg).radian, equp.dec.wrap_at(180*u.deg).radian, alpha=0.5, zorder=1, color='tab:green', linestyle='--')
+        ax.plot(ax_factor*equm.ra.wrap_at(180*u.deg).radian, equm.dec.wrap_at(180*u.deg).radian, alpha=0.5, zorder=1, color='tab:green', linestyle='--')
        
-
-#    ax.scatter(ax_factor*sources.ra.wrap_at(180*u.deg).radian, sources.dec.wrap_at(180*u.deg).radian,s=size*2)
     ind0 = np.where(size < 0.5)
     ind1 = np.where((size >= 0.5) & (size < 1.0))
     ind2 = np.where((size >= 1.0) & (size < 3.0))
@@ -105,11 +66,11 @@ def plot_equ_sky_scatter(sources,size=None,colorInd=None,file_out=None,isshow=Tr
     ax.scatter(ax_factor*sources.ra.wrap_at(180*u.deg).radian[ind3], sources.dec.wrap_at(180*u.deg).radian[ind3],s=size[ind3]*20,marker='x', color='purple')
     # -------------------------------------------------------------------------
     xt0 = ax_factor*np.array([-135,-90,-45,0,45,90,135])*np.pi/180
-    xtl0 =         ['15$^h$','18$^h$','21$^h$','0$^h$','3$^h$','6$^h$','9$^h$']
+    xtl0 =         ['15$^\mathrm{h}$','18$^\mathrm{h}$','21$^\mathrm{h}$','0$^\mathrm{h}$','3$^\mathrm{h}$','6$^\mathrm{h}$','9$^\mathrm{h}$']
     xt1 = ax_factor*np.array([-150,-120,-90,-60,-30,0,30,60,90,120,150])*np.pi/180
-    xtl1 =         ['14$^h$','16$^h$','18$^h$','20$^h$','22$^h$','0$^h$','2$^h$','4$^h$','6$^h$','8$^h$','10$^h$']
+    xtl1 =         ['14$^\mathrm{h}$','16$^\mathrm{h}$','18$^\mathrm{h}$','20$^\mathrm{h}$','22$^\mathrm{h}$','0$^\mathrm{h}$','2$^\mathrm{h}$','4$^\mathrm{h}$','6$^\mathrm{h}$','8$^\mathrm{h}$','10$^\mathrm{h}$']
     yt = np.array([-75,-60,-45,-30,-15,0,15,30,45,60,75])*np.pi/180
-    ytl = ['-75$^{\circ}$','-60$^{\circ}$','-45$^{\circ}$','-30$^{\circ}$','-15$^{\circ}$','0$^{\circ}$','15$^{\circ}$','30$^{\circ}$','45$^{\circ}$','60$^{\circ}$','']
+    ytl = ['-75°','-60°','-45°','-30°','-15°','0°','15°','30°','45°','60°','']
     ax.set_xticks(xt1)
     ax.set_xticklabels(xtl1,fontsize=fontsize)
     ax.set_yticks(yt)
@@ -120,7 +81,6 @@ def plot_equ_sky_scatter(sources,size=None,colorInd=None,file_out=None,isshow=Tr
     ax.set_title('VLA Ku-band Calibrators',fontsize=fontsize)
     ax.set_xlabel('RA (J2000)',fontsize=fontsize)
     ax.set_ylabel('Dec (J2000)',fontsize=fontsize)
-#    ax.set_facecolor('xkcd:battleship grey')
     # -------------------------------------------------------------------------
     plt.tight_layout()
     fig.patch.set_facecolor('white')
@@ -128,6 +88,7 @@ def plot_equ_sky_scatter(sources,size=None,colorInd=None,file_out=None,isshow=Tr
         fig.savefig(file_out, dpi=300,format='png',bbox_inches='tight')
     if isshow:
         plt.show()
+    plt.close()
     return 0
 #===================
 def catalog_to_Skycoord(sour_list):
